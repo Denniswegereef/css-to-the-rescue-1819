864596adbfe556aa459d4ad067b274f7049f5e3e
denniswegereef <denniswegereef@gmail.com>
2019-02-16 15:22:01 +0100

    css

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 import './css/x-app.scss'
+
+//
+// let colorWhite = '#fff'
+// let colorBlack = '#000'
+//
+// setInterval(function() {
+//   flickr(Math.floor(Math.random() * 10) + 1)
+// }, 1000)
+//
+// function flickr(number) {
+//   console.log(number)
+//   setTimeout(function() {
+//     root.style.setProperty('--colorWhite', colorWhite)
+//     root.style.setProperty('--colorBlack', colorBlack)
+//   }, number * 1000)
+// }
